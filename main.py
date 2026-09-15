d6d901798b941582d4600f3162b112dd44c7e45f
kachking-crown <kachi71214@gmail.com>
2026-09-15 14:21:23 +0100

    Update model loading to use new weights file and remove old model

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app.add_middleware(
 
 # --- 2. LOAD THE MODEL ---
 print("Loading model...")
-model = tf.keras.models.load_model('nigerian_food_model.keras')
+model = tf.keras.models.load_model('nigerian_food_weights.weights.h5')
 preprocess_input = tf.keras.applications.mobilenet_v2.preprocess_input
 print("Model loaded successfully!")
 
